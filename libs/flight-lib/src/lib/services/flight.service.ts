f69a85c2350c2a41d4750354b5a3938345f3af0a
Alex Thalhammer <alex@angular-software.at>
2021-05-24 16:30:57 +0200

    Add observer demo

--- libs/flight-lib/src/lib/services/flight.service.ts
+++ libs/flight-lib/src/lib/services/flight.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { ConnectableObservable, Observable, pipe } from 'rxjs';
 import { Flight } from '../models/flight';
-import { delay } from 'rxjs/operators';
+import { delay, publish } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,12 +18,45 @@ export class FlightService {
   constructor(private http: HttpClient) {}
 
   load(from: string, to: string, urgent: boolean): void {
-    this.find(from, to, urgent).subscribe(
+    // 1 // no subscription
+    // this.find(from, to, urgent);
+
+    /*this.find(from, to, urgent).subscribe(
       (flights) => {
         this.flights = flights;
       },
       (err) => console.error('Error loading flights', err)
-    );
+    );*/
+
+    // 2 // 2 subscriptions
+    /*this.find(from, to, urgent).subscribe(
+      (flights) => {
+        this.flights = flights;
+      },
+      (err) => console.error('Error loading flights', err)
+    );*/
+
+    // 3 // publish & connect
+    /*const o = this.find(from, to, urgent).pipe(publish()) as ConnectableObservable<Flight[]>;
+    o.connect();*/
+
+    // 4 // 2 subscriptions
+    /*o.subscribe(
+      (flights) => {
+        this.flights = flights;
+      },
+      (err) => console.error('Error loading flights', err)
+    );*/
+
+    // 1
+    this.find(from, to, urgent)
+      .pipe(delay(this.reqDelay))
+      .subscribe(
+        (flights) => {
+          this.flights = flights;
+        },
+        (err) => console.error('Error loading flights', err)
+      );
   }
 
   find(from: string, to: string, urgent: boolean = false): Observable<Flight[]> {
@@ -47,8 +80,7 @@ export class FlightService {
   }
 
   findById(id: string): Observable<Flight> {
-    const reqObj = { params: null };
-    reqObj.params = new HttpParams().set('id', id);
+    const reqObj = { params: new HttpParams().set('id', id) };
     const url = [this.baseUrl, 'flight'].join('/');
     return this.http.get<Flight>(url, reqObj);
     // return of(flights[0]).pipe(delay(this.reqDelay))
